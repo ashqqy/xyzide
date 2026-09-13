1a11cd901a688cbd0e3ea2af8136c478aa1483c5
artiom <artiom.shaff07@gmail.com>
2026-09-13 17:08:57 +0300

    xyzide default layout

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Xyzide: scalable terminal IDE";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "xyzide";
+        version = "1.0.0";
+
+        src = ./.;
+
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+
+        installPhase = ''
+          mkdir -p $out/bin $out/share/xyzide
+          cp -r configs $out/share/xyzide/
+          cp scripts/yazi-opener.sh $out/bin/yazi-opener
+          cp scripts/xyzide.sh $out/bin/xyzide
+          chmod +x $out/bin/*
+
+          wrapProgram $out/bin/xyzide \
+            --prefix PATH : "$out/bin" \
+            --set XYZIDE_SHARE "$out/share/xyzide" \
+            --set YAZI_CONFIG_HOME "$out/share/xyzide/configs/yazi" \
+            --set LAYOUT_PATH "$out/share/xyzide/configs/layouts/default.kdl"
+        '';
+      };
+    };
+}
